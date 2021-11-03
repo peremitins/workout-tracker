32fd86b9bc755022936cb001e2ea32b085bdb880
nikolay_peremitin <peremitinns@mail.ru>
2021-11-03 14:46:09 +0300

    create workoutApp

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="min-h-full font-Poppins box-border">
+		<Navigation></Navigation>
+		<router-view />
+	</div>
+</template>
+
+<script>
+import Navigation from "./components/Navigation";
+export default {
+	components: {
+		Navigation
+	},
+	setup() {
+		return {};
+	}
+};
+</script>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap");
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>Reg</div>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+	setup() {
+		const email = ref(null);
+		const password = ref(null);
+		const confirmPassword = ref(null);
+		const errorMessage = ref(null);
+
+		return {
+			email,
+			password,
+			confirmPassword,
+			errorMessage
+		};
+	}
+};
+</script>
+
+<style>
+</style>
